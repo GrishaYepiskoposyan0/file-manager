e11ab8107e4e04c1ec4d90e345b57b7ea9e0ebaa
GrishaYepiskoposyan0 <72666584+GrishaYepiskoposyan0@users.noreply.github.com>
2020-10-11 21:20:33 +0400

    File Manager

---
+++ FileManager/Events.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+namespace FileManager
+{
+    public class Events
+    {
+        public void FolderBrowser(ref System.Windows.Controls.TextBox tb)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowNewFolderButton = true;
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tb.Text = fbd.SelectedPath;
+            }
+            
+        }
+        public void Move(string file, System.Windows.Controls.TextBox way, ref string newFolderName)
+        {
+            string extension = Path.GetExtension(file);
+            string[] audio = new string[] { ".MP3", ".WMA", ".AAC", ".WAV", ".FLAC", ".ALAC", ".AIFF", ".DSD", ".PCM", };
+            string[] video = new string[] { ".MP4", ".MOV", ".WMV", ".FLV", ".AVI", ".MKV", ".AVCHD" };
+            string[] photo = new string[] { ".JPEG", ".JPG", ".PNG", ".GIF" };
+            string[] installer = new string[] { ".MSI", ".MSM", ".MSP", ".MST", ".IDT", ".EXE", ".CUB", "P.CP",".TORRENT" ,".ISO"};
+            string[] archives = new string[] { ".ZIP", ".RAR", ".7Z" };
+            string[] doc = new string[] { ".DOC", ".DOCX", ".XLSX", ".XLX", ".PDF", ".XML",".XAML",".TXT"};
+            if (Array.IndexOf(audio, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Audio"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Audio");
+                }
+                newFolderName = "Audio";
+            }
+            else if (Array.IndexOf(video, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Video"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Video");
+                }
+                newFolderName = "Video";
+            }
+            else if (Array.IndexOf(photo, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Photo"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Photo");
+                }
+                newFolderName = "Photo";
+            }
+            else if (Array.IndexOf(installer, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Installers"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Installers");
+                }
+                newFolderName = "Installers";
+            }
+            else if (Array.IndexOf(archives, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Archives"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Archives");
+                }
+                newFolderName = "Archives";
+            }
+            else if (Array.IndexOf(doc, extension.ToUpper()) >= 0)
+            {
+                if (Directory.Exists(way.Text + @"\Documents"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Documents");
+                }
+                newFolderName = "Documents";
+            }
+            else
+            {
+                if (Directory.Exists(way.Text + @"\Other"))
+                {
+                }
+                else
+                {
+                    Directory.CreateDirectory(way.Text + @"\Other");
+                }
+                newFolderName = "Other";
+            }
+        }
+        
+    }
+}
---
+++ FileManager/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+
+
+namespace FileManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void sortFolder_Click(object sender, RoutedEventArgs e)
+        {
+            content.Children.Clear();
+            Events events = new Events();
+            Button sortBrowseButton = new Button
+            {
+                Width = 80,
+                Height = 38,
+                Content = "Browse",
+                Background = header.Background,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(0, 30, 100, 0),
+            };
+            Button sortButton = new Button
+            {
+                Width = 80,
+                Height = 38,
+                Content = "Sort",
+                Background = header.Background,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(0, 100, 100, 0),
+            };
+
+            TextBox sortFolderWayBox = new TextBox
+            {
+                Width = 400,
+                Height = 40,
+                FontSize = 30,
+                Margin = new Thickness(0, -235, 150, 0),
+            };
+            sortBrowseButton.Click += (s, e) =>
+            {
+                events.FolderBrowser(ref sortFolderWayBox);
+            };
+            sortButton.Click += (s, e) =>
+            {
+                string newFolderName = "";
+
+                string[] files = Directory.GetFiles(sortFolderWayBox.Text);
+                foreach (string file in files)
+                {
+                    FileInfo f = new FileInfo(file);
+                    events.Move(file, sortFolderWayBox, ref newFolderName);
+                    f.MoveTo($"{sortFolderWayBox.Text}\\{newFolderName}\\{f.Name}");
+                }
+                MessageBox.Show("File is moved successfuly!");
+            };
+            content.Children.Add(sortBrowseButton);
+            content.Children.Add(sortFolderWayBox);
+            content.Children.Add(sortButton);
+        }
+
+
+    }
+}
